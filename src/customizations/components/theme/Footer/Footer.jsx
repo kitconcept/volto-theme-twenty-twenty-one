84c754c3cc5137f2c7a5e9a30cdd1fd95549bdd5
anrakit <rauch@kitconcept.com>
2021-03-12 07:11:47 +0100

    adding the new footer theme

---
+++ src/customizations/components/theme/Footer/Footer.jsx
@@ -0,0 +1,79 @@
+/**
+ * Footer component.
+ * @module components/theme/Footer/Footer
+ */
+
+import React from 'react';
+import { Container, List, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+import IconFbook from '../../../../../theme/assets/facebook.svg';
+import IconTwit from '../../../../../theme/assets/twitter.svg';
+import IconInst from '../../../../../theme/assets/instagram.svg';
+import IconMail from '../../../../../theme/assets/mail.svg';
+
+const messages = defineMessages({
+  copyright: {
+    id: 'Copyright',
+    defaultMessage: 'Copyright',
+  },
+});
+
+/**
+ * Component to display the footer.
+ * @function Footer
+ * @param {Object} intl Intl object
+ * @returns {string} Markup of the component
+ */
+const Footer = ({ intl }) => (
+  <Segment
+    role="contentinfo"
+    vertical
+    padded
+    inverted
+    color="white"
+    textAlign="center"
+    id="footer"
+  >
+    <Container>
+      <Segment basic inverted color="white" className="discreet">
+        <div className="footer-row1">
+          <a href="#">
+            <img className="sm-icon" src={IconFbook} alt="facebook icon" />
+          </a>
+          <a href="#">
+            <img className="sm-icon" src={IconTwit} alt="twitter icon" />
+          </a>
+          <a href="#">
+            <img className="sm-icon" src={IconInst} alt="instagram icon" />
+          </a>
+          <a href="#">
+            <img className="sm-icon" src={IconMail} alt="e-mail icon" />
+          </a>
+        </div>
+        <div className="footer-row2">
+          <h2>Twenty Twenty-One</h2>
+          <p>
+            Proudly powered by{' '}
+            <a className="link-1" href="#">
+              kitconcept.
+            </a>
+          </p>
+        </div>
+      </Segment>
+    </Container>
+  </Segment>
+);
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+Footer.propTypes = {
+  /**
+   * i18n object
+   */
+};
+
+export default injectIntl(Footer);
